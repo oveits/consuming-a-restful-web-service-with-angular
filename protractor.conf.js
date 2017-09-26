fffeef2623996bf134baa7c369b2aa30cd8a9599
vagrant <vagrant@ubuntu-trusty64-docker-2017-02>
2017-09-26 20:56:53 +0000

    protractor.conf.js: added useAllAngular2AppRoots: true for avoiding sync problems

--- protractor.conf.js
+++ protractor.conf.js
@@ -13,6 +13,7 @@ exports.config = {
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
+  useAllAngular2AppRoots: true,
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
