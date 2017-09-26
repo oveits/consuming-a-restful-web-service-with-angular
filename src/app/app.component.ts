fb82182aab4a9c85065be3880817e43c6d787653
vagrant <vagrant@ubuntu-trusty64-docker-2017-02>
2017-09-26 20:58:14 +0000

    app component: defined missing title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
+  title : any = null
+
   constructor() { }
 
   ngOnInit() {
+     this.title = "app";
   }
 
 }
