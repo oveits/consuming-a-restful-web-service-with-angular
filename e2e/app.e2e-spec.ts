0fa4697b647c3cec9b8696ffabad5dd0eb80706f
vagrant <vagrant@ubuntu-trusty64-docker-2017-02>
2017-10-04 09:52:07 +0000

    corrected matcher from toEqual -> toContain. Added a second matcher toMatch(/^.../)

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -33,7 +33,8 @@ describe('Blog', () => {
   });
 
   it('should display the blog content', () => {
-    expect(blog_content.getText()).toEqual('In this hello world style tutorial, we will follow a step by step guide to a working Angular 4 application.');
+    expect(blog_content.getText()).toContain('In this hello world style tutorial, we will follow a step by step guide to a working Angular 4 application.');
+    expect(blog_content.getText()).toMatch('/^In this hello world style tutorial, we will follow a step by step guide to a working Angular 4 application..*/');
   });
 
 });
