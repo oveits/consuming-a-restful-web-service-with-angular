a73bc577be572378a57ed7fed298397887d2f316
vagrant <vagrant@ubuntu-trusty64-docker-2017-02>
2017-10-04 09:44:39 +0000

    added test for blog content; improved readability of IDs blogtitle -> blog_title

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -24,11 +24,16 @@ describe('Blog', () => {
     browser.get('/');
   });
 
-  const blogtitle = element(by.id('blogtitle'));
+  const blog_title = element(by.id('blog_title'));
+  const blog_content = element(by.id('blog_content'));
 
-  it('should display the blog title', () => {
+  it('should display the blog title as header 1 and id="blog_title"', () => {
     expect(element(by.css('h1')).getText()).toEqual('Angular 4 Hello World Quickstart');
-    expect(blogtitle.getText()).toEqual('Angular 4 Hello World Quickstart');
+    expect(blog_title.getText()).toEqual('Angular 4 Hello World Quickstart');
+  });
+
+  it('should display the blog content', () => {
+    expect(blog_content.getText()).toEqual('In this hello world style tutorial, we will follow a step by step guide to a working Angular 4 application.');
   });
 
 });
