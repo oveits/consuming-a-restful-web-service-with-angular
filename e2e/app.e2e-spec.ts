f2bff0eeb915b9eda968974f7f766170f3c52acd
vagrant <vagrant@ubuntu-trusty64-docker-2017-02>
2017-10-05 21:25:57 +0000

    2.1 add test for blog content (failed)

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -22,9 +22,14 @@ describe('Blog', () => {
   });
 
   const blog_title = element(by.id('blog_title'));
+  const blog_content = element(by.id('blog_content'));
 
   it('should display the blog title as header 1 and id="blog_title"', () => {
     expect(element(by.css('h1')).getText()).toEqual('Angular 4 Hello World Quickstart');
     expect(blog_title.getText()).toEqual('Angular 4 Hello World Quickstart');
+  });
+
+  it('should display the blog content', () => {
+    expect(blog_content.getText()).toContain('In this hello world style tutorial, we will follow a step by step guide to a working Angular 4 application.');
   });
 });
